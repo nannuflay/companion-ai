0a3c2e58763c9cfb9b98de6ab72e34fde74e1b69
nannuflay <mohameddcharafi@gmail.com>
2023-08-01 13:15:28 +0100

    integrate Prisma and add Category Model

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,11 +1,13 @@
-import React from "react";
-
+import { Categories } from "@/components/categories";
 import SearchInput from "@/components/search-input";
+import prismaDb from "@/lib/prismadb";
 
-const RootPage = () => {
+const RootPage = async () => {
+  const categories = await prismaDb.category.findMany();
   return (
     <div className=" h-full p-4 space-y-2">
       <SearchInput />
+      <Categories data={categories} />
     </div>
   );
 };
