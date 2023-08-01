eb51536ab986478849ead1629d0822cfdd4c3e3b
nannuflay <mohameddcharafi@gmail.com>
2023-08-01 12:29:21 +0100

    add a custom debounce hook, implement search bar

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,11 +1,11 @@
-import { UserButton } from "@clerk/nextjs";
 import React from "react";
+
+import SearchInput from "@/components/search-input";
 
 const RootPage = () => {
   return (
-    <div>
-      RootPage
-      <UserButton afterSignOutUrl="/" />
+    <div className=" h-full p-4 space-y-2">
+      <SearchInput />
     </div>
   );
 };
